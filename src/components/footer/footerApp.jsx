a6914eb0bcc6a8581c1cab3ec04b9442f138d14a
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-10-29 18:27:19 +0530

    added styling in the footer section of the movie app

--- src/components/footer/footerApp.jsx
+++ src/components/footer/footerApp.jsx
@@ -13,15 +13,16 @@ const FooterSection = () => {
     return (
         <>
             <footer className="footer-section" style={{ backgroundColor: '#2d313a' }}>
-                <img src="https://cdn-icons-png.flaticon.com/128/1946/1946430.png" alt="" height={'37px'} style={{ cursor: "pointer" }} onClick={() => {
+                <img src="https://cdn-icons-png.flaticon.com/128/1946/1946430.png" alt="" height={'37px'} style={{ cursor: "pointer", padding: "5px" }} onClick={(e) => {
                     navigate("/");
-                }} />
-                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/TV-icon-2.svg/1024px-TV-icon-2.svg.png" alt="" height={'37px'} style={{ cursor: "pointer" }} onClick={() => {
+
+                }} id={`${location.pathname === '/' ? 'active' : ""}`} />
+                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/TV-icon-2.svg/1024px-TV-icon-2.svg.png" alt="" height={'37px'} style={{ cursor: "pointer", padding: "5px" }} onClick={() => {
                     navigate("/webSeries");
-                }} />
-                <img src="https://www.iconpacks.net/icons/1/free-movie-icon-850-thumb.png" alt="" height={'37px'} style={{ cursor: "pointer" }} onClick={() => {
+                }} id={`${location.pathname === '/webSeries' ? 'active' : ""}`} />
+                <img src="https://www.iconpacks.net/icons/1/free-movie-icon-850-thumb.png" alt="" height={'37px'} style={{ cursor: "pointer", padding: "5px" }} onClick={() => {
                     navigate("/movies");
-                }} />
+                }} id={`${location.pathname === '/movies' ? 'active' : ""}`} />
                 {/* <Link to="/movies" className="footer-element" id={location.pathname === '/movies' ? 'active' : ''}>Movies</Link>
                 <Link to="/webSeries" className="footer-element" id={location.pathname === '/webSeries' ? 'active' : ''}>Web series</Link> */}
             </footer>
