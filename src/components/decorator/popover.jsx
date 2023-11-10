b75b4a24a6a0498d31aa252c27c27ffa74ae8f25
Rutvik Jaiswal <rutvikjaiswal195@gmail.com>
2023-11-11 00:17:48 +0530

    added movies downloads links to the movies app

--- src/components/decorator/popover.jsx
+++ src/components/decorator/popover.jsx
@@ -2,16 +2,49 @@ import * as React from 'react';
 import Popover from '@mui/material/Popover';
 import "./popover.css"
 export default function BasicPopover({ anchorEl, setAnchorEl, movie_json, ytLinkKey }) {
-    // const [anchorEl, setAnchorEl] = React.useState(null);
+    const [anchorElDownload, setAnchorElDownload] = React.useState(null);
+    const [moviesDownloadLink, setMoviesDownloadLinks] = React.useState([])
+    const moviesDownloadLinks = async () => {
+        try {
+            const movieName = `${movie_json.title ? movie_json.title : movie_json.name}`.toLowerCase().replace(/\s/g, '-')
+            const response = await fetch(
+                `https://pogolinks.hair/${movie_json.media_type === 'movie' ? 'movies' : 'webseries'}/${movieName}/`
+            );
 
-    // const handleClick = (event) => {
-    //     setAnchorEl(event.currentTarget);
-    // };
+            if (!response.ok) {
+                throw new Error("Network response was not ok.");
+            }
+
+            // Depending on the type of response (JSON, text, etc.), you can use appropriate methods
+            const responseData = await response.text(); // Change this based on the response type
+
+            const parser = new DOMParser();
+            const htmlDocument = parser.parseFromString(responseData, "text/html");
+
+            const findingURL_fromDoc = htmlDocument.querySelectorAll(
+                "tr.table-sub .maintb a"
+            );
+            // console.log("This is movies downloads URLS")
+            const moviesDownloadLinks = Array.from(findingURL_fromDoc).map((val) => {
+                return ([val.href, val.innerHTML]);
+            });
+            setMoviesDownloadLinks(moviesDownloadLinks);
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
+    React.useEffect(() => {
+
+        moviesDownloadLinks()
+    }, [])
 
     const handleClose = () => {
         setAnchorEl(null);
     };
 
+    const handleClickDownload = (event) => {
+        setAnchorElDownload(event.currentTarget);
+    };
     const open = Boolean(anchorEl);
     const id = open ? 'simple-popover' : undefined;
 
@@ -42,14 +75,66 @@ export default function BasicPopover({ anchorEl, setAnchorEl, movie_json, ytLink
                         <p className="description" style={{ textAlign: "justify" }}>
                             {movie_json.overview}
                         </p>
-                        <div className="youtube-link">
+                        <div className="youtube-link flex flex-gap-10px">
                             {ytLinkKey && <a href={`https://www.youtube.com/watch?v=${ytLinkKey}`} tabIndex={0} target="_blank" rel="noopener noreferrer" className="yt-link"> Please Watch it</a>}
+
+                            <span onClick={(e) => {
+                                handleClickDownload(e)
+                                handleClose()
+                            }} style={{ cursor: "pointer" }} className="yt-link">Download Movie</span>
                         </div>
 
                     </div>
                 </div>
 
             </Popover>
+            <DownloadPopOver movieDownloadLinks={moviesDownloadLink} anchorElDownload={anchorElDownload} setAnchorElDownload={setAnchorElDownload} />
+
+        </div >
+    );
+}
+
+
+function DownloadPopOver({ movieDownloadLinks, anchorElDownload, setAnchorElDownload }) {
+
+    console.log("====>>>>>>>>>>>", movieDownloadLinks)
+    // moviesDownloadLinks()
+
+    const handleClosing = () => {
+        setAnchorElDownload(null);
+    };
+
+
+    const open = Boolean(anchorElDownload);
+    const id = open ? 'simple-popover' : undefined;
+    return (
+        <div >
+            <Popover
+                id={id}
+                open={open}
+                anchorEl={anchorElDownload}
+                onClose={handleClosing}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                transformOrigin={{
+                    vertical: "top",
+                    horizontal: "right"
+                }}
+                className='popover-width'
+            >
+                <div className="container-moview-detainls flex-col width-fit-content" style={{ width: "fit-content" }}>
+
+                    {movieDownloadLinks && movieDownloadLinks.map((val) => {
+                        return (<>
+                            <a className="movie-download-link" target='_blank' href={`${val[0]}`}>{val[1]}</a>
+                        </>)
+                    })}
+                </div>
+
+            </Popover>
         </div>
     );
+
 }
